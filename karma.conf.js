b1209aef83dc12cb2d7ac0684d41e1e37e5b28e3
senbin.zhuang <flow@flow-2.local>
2017-08-04 11:56:16 +0800

    修改webpack配置

--- karma.conf.js
+++ karma.conf.js
@@ -74,15 +74,10 @@ module.exports = function(config) {
                 {
                     test: /\.js$/,
                     exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: [['es2015', {module: false}]],
-                                cacheDirectory: true
-                            }
-                        }
-                    ]
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
                 }
             ]
         }
