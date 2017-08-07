413a86ea61cab42815c74f975d9ea2829589dd29
senbin.zhuang <flow@flow-2.local>
2017-08-07 16:35:36 +0800

    添加金额分割

--- src/demo.js
+++ src/demo.js
@@ -1,5 +1,35 @@
 module.exports = {
-    add: function (a, b) {
+    add: function(a, b) {
         return a + b
+    },
+    // 金钱加千分号
+    cut: function(money) {
+        let m = Number(money),
+            result = [];
+        const base = 1000;
+
+        function fn(num) {
+            let len = 0,
+                str = num.toString();
+
+            try{
+                len = str.split('.')[1].length
+            } catch(e) {
+                // not deal
+            }
+            
+            return Number(str.replace('.', '')) % (base * Math.pow(10, len)) / Math.pow(10, len)
+        }
+
+        if (isNaN(m)) return 0;
+
+        while(m >= base) {
+            result.unshift(fn(m));
+            m = parseInt(m / base);
+        }
+
+        result.unshift(fn(m));
+
+        return result.join(',')
     }
 }
--- test/demo.js
+++ test/demo.js
@@ -4,4 +4,7 @@ describe('test demo', () => {
     it('add function', () => {
         expect(demo.add(1, 2)).toEqual(3)
     })
+    it('cut function', () => {
+        expect(demo.cut(12345678.02)).toEqual('12,345,678.02')
+    })
 })
